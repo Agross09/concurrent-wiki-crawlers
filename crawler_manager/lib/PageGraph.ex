679a6e61828afe0804c449ecf929108ee8b2083d
Andrew Gross <aleogross@gmail.com>
2019-12-01 16:10:59 -0500

    Created GenServer versions of PageGraph and PageQueue modules

--- crawler_manager/lib/PageGraph.ex
+++ crawler_manager/lib/PageGraph.ex
@@ -1,14 +1,120 @@
-defmodule PageGraph do
-    def put_edge(originPage, nextPage, pageGraph) do
-        if Enum.member?(Map.keys(pageGraph), originPage) do
-            neighbors = Map.fetch!(pageGraph, originPage)
-            new_neighbors = [nextPage | neighbors]
-            Map.replace!(pageGraph, originPage, new_neighbors)
-        else
-            Map.put(pageGraph, originPage, [nextPage])
-        end
+defmodule CrawlerManager.PageGraph do
+  use GenServer
+
+  @moduledoc """
+  Documentation for CrawlerManager.PageGraph
+
+  Each instance of this module is a GenServer that facilitates a graph.
+  This graph is the state of the subgraph of Wikipedia pages received from the
+  python crawlers.
+  """
+
+  @moduledoc """
+  External API for CrawlerManager.PageGraph
+  """
+
+  @doc """
+  Function: start_link/1
+
+  Initialization of a pageGraph process and its state.
+
+  Given a page (url string), start a GenServer process and map of lists for
+  each page and its neighbors (list of url strings).
+  """
+  def start_link(originPage) when is_string(originPage) do
+    pageGraph = %{originPage => []}
+    {:ok, _pid} = GenServer.start_link(__MODULE__, pageGraph)
+  end
+
+  @doc """
+  Function: put_edge/3
+
+  Function to add an edge between existing page and neighbor page.
+
+  Given a pid of pageGraph, url string of existing page, and url string of
+  neighboring page, CAST a change of state for the pageGraph by adding an
+  edge between the two given pages.
+  """
+  def put_edge(graph_pid, originPage, nextPage) do
+    GenServer.cast(graph_pid, {:put_edge, originPage, nextPage})
+  end
+
+  @doc """
+  Function: is_duplicate/2
+
+  Function to check if page already exists in the pageGraph.
+
+  Given pid of pageGraph and a url string of a page, CALL a query to check
+  if the given page is an exisiting node in pageGraph. Function replies
+  with a boolean True or False and does not change the pageGraph state.
+  """
+  def is_duplicate(graph_pid, page) do
+    GenServer.call(graph_pid, {:is_duplicate, page})
+  end
+
+
+  @doc """
+  Function: add_subgraph/2
+
+  Function to add urls explored by the crawler and their neighbors to the
+  pageGraph graph.
+
+  Given a pid of pageGraph and a map of url strings of explored pages as keys
+  and their neighboring url strings as values, use put_edge/3 to add edges
+  between the key url strings and their neighbors and add this subgraph
+  to the graph pageGraph.
+  """
+  def add_subgraph(graph_pid, urls_subgraph) when is_map(urls_subgraph) do
+    Enum.map(
+      urls_subgraph,
+      fn {base_url, neighbors} ->
+        Enum.map(
+          neighbors,
+          fn url ->
+            put_edge(graph_pid, base_url, url)
+          end
+        )
+      end
+    )
+  end
+  #############################################################################
+  # GenServer Implementation
+
+  # GenServer handle_cast for putting an edge between two pages in the graph.
+  def handle_cast({:put_edge, originPage, nextPage}, pageGraph) do
+    updatedGraph = put_edge_aux(originPage, nextPage, pageGraph)
+    {:noreply, updatedGraph}
+  end
+
+  # GenServer handle_call for checking if a page exists
+  # as a key in the pageGraph.
+  def handle_call({:is_duplicate, page}, _from, pageGraph) do
+    {:reply, is_duplicate_aux(page, pageGraph), pageGraph}
+  end
+
+  #############################################################################
+  # Helper functions
+
+  # FUNCTION: put_edge_aux/3
+  # Facilitates the putting of a edge between two pages in pageGraph.
+  # Given a url string for a page, a url string for its neighbor,
+  # and the pageGraph map, check to see if the originPage is in the pageGraph
+  # map. If so, change the list of neighbors associated with the originPage in
+  # the pageGraph map, else add originPage to the pageGraph map and add the
+  # nextPage url as the first element in its list of neighbors.
+  defp put_edge_aux(originPage, nextPage, pageGraph) do
+    if Enum.member?(Map.keys(pageGraph), originPage) do
+        neighbors = Map.fetch!(pageGraph, originPage)
+        new_neighbors = [nextPage | neighbors]
+        Map.replace!(pageGraph, originPage, new_neighbors)
+    else
+        Map.put(pageGraph, originPage, [nextPage])
     end
-    def is_duplicate(pageGraph, page) do
-        Enum.member?(Map.keys(pageGraph), page)
-    end
+  end
+
+  # FUNCTION: is_duplicate_aux/2
+  # Checks if a given page (url string) is a key in the pageGraph map.
+  defp is_duplicate_aux(page, pageGraph) do
+    Enum.member?(Map.keys(pageGraph), page)
+  end
 end
--- crawler_manager/lib/PageQueue.ex
+++ crawler_manager/lib/PageQueue.ex
@@ -1,11 +1,92 @@
-defmodule PageQueue do
-    alias PageGraph
-    def get_new_page(pageQueue) do
-        List.pop_at(pageQueue, 0)
+defmodule CrawlerManager.PageQueue do
+  use GenServer
+
+  @moduledoc """
+  Documentation for CrawlerManager.PageQueue
+
+  Each instance of this module is a GenServer that facilitates a queue.
+  This queue stores the pages that the crawlers will eventually explore.
+  """
+
+  @moduledoc """
+  External API for CrawlerManager.PageQueue
+  """
+
+  @doc """
+  Function: start_link/1
+
+  Initialization of a pageQueue process and its state.
+
+  Given a list of pages (url strings), start a GenServer process
+  to hold the state of a queue of pages to explore.
+  """
+  def start_link(urls) when is_list(urls) do
+    {:ok, _pid} = GenServer.start_link(__MODULE__, urls)
+  end
+
+  @doc """
+  Function: get_new_page/1
+
+  Function to dequeue a page from queue pageQueue.
+
+  Given a pid of pageQueue, CALL a change of state for the pageQueue
+  by dequeuing a page from pageQueue queue and return the page.
+  """
+  def get_new_page(queue_pid) do
+    GenServer.call(queue_pid, :get_new_page)
+  end
+
+  @doc """
+  Function: put_page/3
+
+  Function to enqueue a page into queue pageQueue.
+
+  Given a new page (url string), a pid of a pageQueue process, and
+  a pid of a pageGraph process, CAST a change of state to the pageQueue
+  to add the given page to the queue of pages to explore. Function does
+  not add page to pageQueue queue if the given page exists as a key in
+  the graph pageGraph.
+  """
+  def put_page(new_page, queue_pid, graph_pid) do
+    GenServer.cast(queue_pid, {:put_page, new_page, graph_pid})
+  end
+
+  #############################################################################
+  # GenServer Implementation
+
+  # GenServer handle_call for getting a new page from the queue pageQueue.
+  def handle_call(:get_new_page, _from, pageQueue) do
+    {page, updatedQueue} = get_new_page_aux(pageQueue)
+    {:reply, page, updatedQueue}
+  end
+
+  # GenServer handle_cast for putting a new page into the queue pageQueue.
+  # Checks to see if the page is unexplored (not a key in graph pageGraph).
+  def handle_cast({:put_page, new_page, graph_pid}, pageQueue) do
+    updatedQueue = put_page_aux(new_page, graph_pid, pageQueue)
+    {:noreply, updatedQueue}
+  end
+
+
+  #############################################################################
+  # Helper functions
+
+  # FUNCTION: get_new_page_aux/1
+  # Dequeues page from queue page Queue.
+  # Returns tuple {page, pageQueue - page}
+  defp get_new_page_aux(pageQueue) do
+    List.pop_at(pageQueue, 0)
+  end
+
+  # FUNCTION: put_page_aux/3
+  # Checks if page exists in graph pageGraph. If so, function returns
+  # the original queue, otherwise, it enqueues the new page to the end of the
+  # queue.
+  # Returns the updated pageQueue.
+  defp put_page_aux(new_page, graph_pid, pageQueue) do
+    case CrawlerManager.PageGraph.is_duplicate(graph_pid, new_page) do
+      true -> pageQueue
+      false -> List.insert_at(pageQueue, -1, new_page)
     end
-    def put_page(new_page, pageQueue, pageGraph) do
-        if !PageGraph.is_duplicate(pageGraph, new_page) do
-            List.insert_at(pageQueue, -1, new_page)
-        end
-    end
+  end
 end
